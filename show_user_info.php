cfa12d651f5d2958a64fdebe57cdb30da70d4831
EdgarACarneiro <up201503784@fe.up.pt>
2017-12-01 19:14:56 +0000

    getting proper images

--- show_user_info.php
+++ show_user_info.php
@@ -5,14 +5,17 @@ include_once('database/user.php');
 $user = getUserInfo($_SESSION['username']);
 
 function showInformation($user) {
+    $username = $user['username'];
+    $imgPath = ("images/user/" . $username . ".png");
+
     echo '<section class="profile-container" id="profile">';
     echo '<div class="profile-pic">';
-    //TODO
-    echo '<img src="images/user/default.png" alt="user profile picture" 
-            width="300" height="300">';
+    echo '<img src=' . 
+        (file_exists($imgPath) ? $imgPath : "images/user/default.png").
+        ' alt="user profile picture">';
     echo '</div>';
     echo '<div class="username-info">';
-    echo '<h2>' . $user['username'] . '</h2>';
+    echo '<h2>' . $username . '</h2>';
     echo '<h4>' . $user['about'] . '</h4>';
     echo '</div>';
     echo '<button type="button" class="btn edit-profile">
