addf3c01d2aa0a0b97ad171adf897dc61631f739
João Carvalho <joaocarvalho9@gmail.com>
2017-12-05 23:17:09 +0000

    fix event handler

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,32 +2,41 @@ var masonry_item_class = document.getElementsByClassName('masonry-item');
 
 var feed_container = document.getElementById('feed');
 
-function modal() {
-    if (this.style.position != "absolute") {
-        this.parentElement.style.height = this.clientHeight.toString() + "px";
-        this.style.position = "absolute";
-        this.style.top = this.offsetTop;
-        this.style.left = this.offsetLeft;
-        this.style.transition = "transform .5s ease-in-out";
-        let translateX = ((feed_container.offsetWidth - 500) / 2 - this.offsetLeft);
-        let translateY = (document.documentElement.scrollTop + 200 - this.offsetTop);
-        this.style.transform = "translate(" +
-            translateX.toString() + "px , " +
-            translateY.toString() + "px )";
-        this.style.width = "500px";
-        document.getElementById('modal').style.zIndex = "5";
-        document.getElementById('modal').style.opacity = "1";
-    } else {
+var opened_Model;
+
+function openModal() {
+    this.parentElement.style.height = this.clientHeight.toString() + "px";
+    this.style.position = "absolute";
+    this.style.top = this.offsetTop;
+    this.style.left = this.offsetLeft;
+    this.style.transition = "transform .5s ease-in-out";
+    let translateX = ((feed_container.offsetWidth - 500) / 2 - this.offsetLeft);
+    let translateY = (document.documentElement.scrollTop + 200 - this.offsetTop);
+    this.style.transform = "translate(" +
+        translateX.toString() + "px , " +
+        translateY.toString() + "px )";
+    this.style.width = "500px";
+    document.getElementById('modal').style.zIndex = "5";
+    document.getElementById('modal').style.opacity = "1";
+    document.getElementById('modal').addEventListener("click", closeModal);
+    this.removeEventListener("click", openModal);
+    opened_Model = this;
+}
+
+function closeModal() {
+    if (opened_Model != null) {
         document.getElementById('modal').style.opacity = "0";
         document.getElementById('modal').style.zIndex = "-1";
-        this.parentElement.style.height = "";
-        this.style.position = "";
-        this.style.width = "";
-        this.style.transform = "";
-        this.style.transition = "";
+        opened_Model.parentElement.style.height = "";
+        opened_Model.style.position = "";
+        opened_Model.style.width = "";
+        opened_Model.style.transform = "";
+        opened_Model.style.transition = "";
+        opened_Model.addEventListener("click", openModal);
+        opened_Model = null;
     }
 }
 
 Array.from(masonry_item_class).forEach(function(element) {
-    element.lastChild.addEventListener('click', modal);
+    element.lastChild.addEventListener('click', openModal);
 });
