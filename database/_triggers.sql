5e5a8476d845fd820bb071ccdb906b64dbb56865
AndreFCruz <andrecruz97@gmail.com>
2017-12-11 19:19:28 +0000

    ON DELETE CASCADE - the magic of sql :smile:

--- database/_triggers.sql
+++ database/_triggers.sql
@@ -1,13 +1,8 @@
 DROP TRIGGER if exists CheckTaskDate;
-
-BEGIN TRANSACTION;
-
 CREATE TRIGGER CheckTaskDate
 BEFORE INSERT ON Task
 FOR EACH ROW
-WHEN New.duedate < (SELECT date('now'))
+WHEN NEW.duedate < (SELECT date('now'))
 BEGIN
     SELECT raise(rollback, "Task's duedate must be in the future");
 END;
-
-COMMIT TRANSACTION;
