05fd9587e2d2786053026274a40168398db01eff
AndreFCruz <andrecruz97@gmail.com>
2017-12-12 11:18:36 +0000

    a caminho do 20

--- database/_triggers.sql
+++ database/_triggers.sql
@@ -1,17 +1,24 @@
-DROP TRIGGER if exists CheckTaskDate;
-CREATE TRIGGER CheckTaskDate
-BEFORE INSERT ON Task
-FOR EACH ROW
-WHEN NEW.duedate < (SELECT date('now'))
-BEGIN
-    SELECT raise(rollback, "Task's duedate must be in the future");
-END;
-
+-- DROP TRIGGER if exists CheckTaskDate;
+-- CREATE TRIGGER CheckTaskDate
+-- BEFORE INSERT ON Task
+-- FOR EACH ROW
+-- WHEN NEW.duedate < (SELECT date('now'))
+-- BEGIN
+--     SELECT raise(rollback, "Task's duedate must be in the future");
+-- END;
 
 DROP TRIGGER if exists DeleteUserTask;
 CREATE TRIGGER DeleteUserTask
 BEFORE DELETE ON Task
 FOR EACH ROW
 BEGIN
-    DELETE FROM UserTask UT WHERE UT.task_id = OLD.task_id;
+    DELETE FROM UserTask WHERE UserTask.task_id = OLD.task_id;
+END;
+
+DROP TRIGGER if exists DeleteItem;
+CREATE TRIGGER DeleteItem
+BEFORE DELETE ON Task
+FOR EACH ROW
+BEGIN
+    DELETE FROM Item WHERE Item.task_id = OLD.task_id;
 END;
