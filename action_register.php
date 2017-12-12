52405935b94aa940b230e3911605b4143660fb36
AndreFCruz <andrecruz97@gmail.com>
2017-12-12 23:03:29 +0000

    a caminho do 21

--- action_register.php
+++ action_register.php
@@ -1,13 +1,16 @@
 <?php
 include_once('includes/init.php');
+include_once('includes/messages.php');
 include_once('database/user.php');
 
 if (preg_match("/^[\w\d]{4,}$/", $_POST['username']) &&
     (registerUser($_POST['username'], $_POST['email'], $_POST['password']))) {
   setCurrentUser($_POST['username']);
+  setSuccessMessage("SUCCESS: account created.");
   echo json_encode("SUCCESS: account created.");
   die;
 }
 
+setErrorMessage("ERROR: username already taken.");
 echo json_encode("ERROR: username already taken.");
 ?>
